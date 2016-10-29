e9eec79b5870b463a64a86e2bbd25fcbf36afff1
Ben Marshall <ben3005@hotmail.com>
2016-10-29 19:59:32 +0100

    added styling, cleaned up build tasks

--- musical-robot/src/musical-robot/gulpfile.js
+++ musical-robot/src/musical-robot/gulpfile.js
@@ -1,56 +1,59 @@
-﻿/// <binding BeforeBuild='tsCompile' />
+﻿/// <binding BeforeBuild='build' />
 var gulp = require('gulp');
-
+var del = require('del');
+var sass = require('gulp-sass');
 var libs = './wwwroot/libs/';
+var cleanCSS = require('gulp-clean-css');
+var rename = require('gulp-rename');
 
 gulp.task('default', function () {
 	// place code for your default task here
 });
 
-gulp.task('restore:core-js', function () {
+gulp.task('restore:core-js', ['clean:lib'], function () {
 	gulp.src([
         'node_modules/core-js/client/*.js'
 	]).pipe(gulp.dest(libs + 'core-js'));
 });
-gulp.task('restore:zone.js', function () {
+gulp.task('restore:zone.js', ['clean:lib'], function () {
 	gulp.src([
         'node_modules/zone.js/dist/*.js'
 	]).pipe(gulp.dest(libs + 'zone.js'));
 });
-gulp.task('restore:reflect-metadata', function () {
+gulp.task('restore:reflect-metadata', ['clean:lib'], function () {
 	gulp.src([
         'node_modules/reflect-metadata/reflect.js'
 	]).pipe(gulp.dest(libs + 'reflect-metadata'));
 });
-gulp.task('restore:systemjs', function () {
+gulp.task('restore:systemjs', ['clean:lib'], function () {
 	gulp.src([
         'node_modules/systemjs/dist/*.js'
 	]).pipe(gulp.dest(libs + 'systemjs'));
 });
-gulp.task('restore:rxjs', function () {
+gulp.task('restore:rxjs', ['clean:lib'], function () {
 	gulp.src([
         'node_modules/rxjs/**/*.js'
 	]).pipe(gulp.dest(libs + 'rxjs'));
 });
-gulp.task('restore:angular-in-memory-web-api', function () {
+gulp.task('restore:angular-in-memory-web-api', ['clean:lib'], function () {
 	gulp.src([
         'node_modules/angular-in-memory-web-api/**/*.js'
 	]).pipe(gulp.dest(libs + 'angular-in-memory-web-api'));
 });
 
-gulp.task('restore:angular', function () {
+gulp.task('restore:angular', ['clean:lib'], function () {
 	gulp.src([
         'node_modules/@angular/**/*.js'
 	]).pipe(gulp.dest(libs + '@angular'));
 });
 
-gulp.task('restore:bootstrap', function () {
+gulp.task('restore:bootstrap', ['clean:lib'], function () {
 	gulp.src([
         'node_modules/bootstrap/dist/**/*.*'
 	]).pipe(gulp.dest(libs + 'bootstrap'));
 });
 
-gulp.task('restore', [
+gulp.task('build:lib-restore', [
     'restore:core-js',
     'restore:zone.js',
     'restore:reflect-metadata',
@@ -60,6 +63,32 @@ gulp.task('restore', [
     'restore:angular',
     'restore:bootstrap'
 ]);
-gulp.task('tsCompile', [], function () {
+
+gulp.task('build:ts-compile', ['clean:app'], function () {
     gulp.src('app/**/*.js').pipe(gulp.dest('./wwwroot/app/'));
+});
+
+gulp.task('build', ['build:ts-compile', 'build:lib-restore', 'build:sass'], function () {
+
+});
+
+gulp.task('clean:app', [], function () {
+	return del(['./wwwroot/app/**/*']);
+});
+
+gulp.task('clean:lib', [], function () {
+	return del(['./wwwroot/lib/**/*']);
+});
+
+gulp.task('build:sass', [], function () {
+	gulp.src('styling/app.scss')
+        .pipe(sass({
+        	includePaths: [
+				'./node_modules/bootstrap-sass/assets/stylesheets'
+        	],
+        	errorLogToConsole: true
+        }))
+		.pipe(cleanCSS())
+		.pipe(rename({ extname: '.min.css' }))
+		.pipe(gulp.dest('./wwwroot/css'));
 });
